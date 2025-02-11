6f1a4de27e60ea7bb3e628ff141b5dcca65a5b56
Andrea Marie Baikole <117893335+AndreaB54@users.noreply.github.com>
2025-02-11 11:27:41 +0700

    Pertemuan 2

---
+++ #Folder Pertemuan 2/Jackpot/Jackpot/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        lblSatu = New Label()
+        lblDua = New Label()
+        lblTiga = New Label()
+        lblStatus = New Label()
+        Timer1 = New Timer(components)
+        Timer2 = New Timer(components)
+        Timer3 = New Timer(components)
+        Timer4 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(29, 33)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(110, 36)
+        Button1.TabIndex = 0
+        Button1.Text = "Spin!"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' lblSatu
+        ' 
+        lblSatu.BorderStyle = BorderStyle.Fixed3D
+        lblSatu.Font = New Font("Segoe UI", 24F)
+        lblSatu.Location = New Point(37, 106)
+        lblSatu.Name = "lblSatu"
+        lblSatu.Size = New Size(149, 143)
+        lblSatu.TabIndex = 1
+        lblSatu.Text = "A"
+        lblSatu.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblDua
+        ' 
+        lblDua.BorderStyle = BorderStyle.Fixed3D
+        lblDua.Font = New Font("Segoe UI", 24F)
+        lblDua.Location = New Point(203, 106)
+        lblDua.Name = "lblDua"
+        lblDua.Size = New Size(149, 143)
+        lblDua.TabIndex = 2
+        lblDua.Text = "A"
+        lblDua.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblTiga
+        ' 
+        lblTiga.BorderStyle = BorderStyle.Fixed3D
+        lblTiga.Font = New Font("Segoe UI", 24F)
+        lblTiga.Location = New Point(370, 106)
+        lblTiga.Name = "lblTiga"
+        lblTiga.Size = New Size(149, 143)
+        lblTiga.TabIndex = 3
+        lblTiga.Text = "A"
+        lblTiga.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.Font = New Font("Segoe UI", 12F)
+        lblStatus.Location = New Point(37, 304)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(97, 29)
+        lblStatus.TabIndex = 4
+        lblStatus.Text = "Status"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 10
+        ' 
+        ' Timer2
+        ' 
+        Timer2.Interval = 9
+        ' 
+        ' Timer3
+        ' 
+        Timer3.Interval = 11
+        ' 
+        ' Timer4
+        ' 
+        Timer4.Interval = 1000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(560, 354)
+        Controls.Add(lblStatus)
+        Controls.Add(lblTiga)
+        Controls.Add(lblDua)
+        Controls.Add(lblSatu)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Jackpot"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblSatu As Label
+    Friend WithEvents lblDua As Label
+    Friend WithEvents lblTiga As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+
+End Class
---
+++ #Folder Pertemuan 2/MineSweeper_Generator/MineSweeper_Generator/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblBaris = New Label()
+        lblKolom = New Label()
+        lblJmlBom = New Label()
+        nudBaris = New NumericUpDown()
+        nudKolom = New NumericUpDown()
+        nudJmlBom = New NumericUpDown()
+        btnBuat = New Button()
+        Panel1 = New Panel()
+        CType(nudBaris, ComponentModel.ISupportInitialize).BeginInit()
+        CType(nudKolom, ComponentModel.ISupportInitialize).BeginInit()
+        CType(nudJmlBom, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' lblBaris
+        ' 
+        lblBaris.Font = New Font("Segoe UI", 12F)
+        lblBaris.Location = New Point(22, 19)
+        lblBaris.Name = "lblBaris"
+        lblBaris.Size = New Size(67, 28)
+        lblBaris.TabIndex = 0
+        lblBaris.Text = "Baris :"
+        ' 
+        ' lblKolom
+        ' 
+        lblKolom.Font = New Font("Segoe UI", 12F)
+        lblKolom.Location = New Point(22, 63)
+        lblKolom.Name = "lblKolom"
+        lblKolom.Size = New Size(115, 28)
+        lblKolom.TabIndex = 1
+        lblKolom.Text = "Kolom :"
+        ' 
+        ' lblJmlBom
+        ' 
+        lblJmlBom.Font = New Font("Segoe UI", 12F)
+        lblJmlBom.Location = New Point(350, 19)
+        lblJmlBom.Name = "lblJmlBom"
+        lblJmlBom.Size = New Size(143, 28)
+        lblJmlBom.TabIndex = 2
+        lblJmlBom.Text = "Jumlah Bomb :"
+        ' 
+        ' nudBaris
+        ' 
+        nudBaris.Location = New Point(122, 19)
+        nudBaris.Name = "nudBaris"
+        nudBaris.Size = New Size(197, 27)
+        nudBaris.TabIndex = 3
+        ' 
+        ' nudKolom
+        ' 
+        nudKolom.Location = New Point(122, 68)
+        nudKolom.Name = "nudKolom"
+        nudKolom.Size = New Size(197, 27)
+        nudKolom.TabIndex = 4
+        ' 
+        ' nudJmlBom
+        ' 
+        nudJmlBom.Location = New Point(507, 19)
+        nudJmlBom.Name = "nudJmlBom"
+        nudJmlBom.Size = New Size(197, 27)
+        nudJmlBom.TabIndex = 5
+        ' 
+        ' btnBuat
+        ' 
+        btnBuat.Location = New Point(714, 15)
+        btnBuat.Name = "btnBuat"
+        btnBuat.Size = New Size(80, 29)
+        btnBuat.TabIndex = 6
+        btnBuat.Text = "Buat"
+        btnBuat.UseVisualStyleBackColor = True
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Location = New Point(27, 109)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(762, 331)
+        Panel1.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(811, 450)
+        Controls.Add(Panel1)
+        Controls.Add(btnBuat)
+        Controls.Add(nudJmlBom)
+        Controls.Add(nudKolom)
+        Controls.Add(nudBaris)
+        Controls.Add(lblJmlBom)
+        Controls.Add(lblKolom)
+        Controls.Add(lblBaris)
+        Name = "Form1"
+        Text = "MineSweeper Generator"
+        CType(nudBaris, ComponentModel.ISupportInitialize).EndInit()
+        CType(nudKolom, ComponentModel.ISupportInitialize).EndInit()
+        CType(nudJmlBom, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents lblBaris As Label
+    Friend WithEvents lblKolom As Label
+    Friend WithEvents lblJmlBom As Label
+    Friend WithEvents nudBaris As NumericUpDown
+    Friend WithEvents nudKolom As NumericUpDown
+    Friend WithEvents nudJmlBom As NumericUpDown
+    Friend WithEvents btnBuat As Button
+    Friend WithEvents Panel1 As Panel
+
+End Class
